356ef27286238aea8d9749454d83c53e39ea550a
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-26 00:09:39 +0200

    Fixed migration file

--- src/database/2015_10_29_000000_captain_hook_setup_logs_table.php
+++ src/database/2015_10_29_000000_captain_hook_setup_logs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CaptainHookSetupTable extends Migration
+class CaptainHookSetupLogsTable extends Migration
 {
     /**
      * Run the migrations.
