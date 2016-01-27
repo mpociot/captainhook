a3ca3d80abe00b73d7307759b81fbde4f5a94d19
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-27 22:22:21 +0200

    Make sure the second migration always runs after the first

--- src/Mpociot/CaptainHook/CaptainHookServiceProvider.php
+++ src/Mpociot/CaptainHook/CaptainHookServiceProvider.php
@@ -80,7 +80,7 @@ class CaptainHookServiceProvider extends ServiceProvider
     {
         $migrations = [
             __DIR__ . '/../../database/2015_10_29_000000_captain_hook_setup_table.php' => database_path('/migrations/' . date('Y_m_d_His') . '_captain_hook_setup_table.php'),
-            __DIR__ . '/../../database/2015_10_29_000001_captain_hook_setup_logs_table.php' => database_path('/migrations/' . substr(date('Y_m_d_His'), 0, -1).'1_captain_hook_setup_logs.php'),
+            __DIR__ . '/../../database/2015_10_29_000001_captain_hook_setup_logs_table.php' => database_path('/migrations/' . date('Y_m_d_His', strtotime('+1s')).'_captain_hook_setup_logs.php'),
         ];
 
         foreach ($migrations as $migration => $toPath) {
