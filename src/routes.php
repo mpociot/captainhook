11e78aa3e932dbeabfa10d2aee30a3ed85a4b857
Marcel Pociot <m.pociot@gmail.com>
2016-05-06 09:47:34 +0200

    Change auth middleware order

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['auth', 'web']], function ($router) {
+Route::group(['middleware' => ['web', 'auth']], function ($router) {
     $router->get('/settings/api/webhooks', 'Mpociot\CaptainHook\Http\WebhookController@all');
     $router->post('/settings/api/webhook', 'Mpociot\CaptainHook\Http\WebhookController@store');
     $router->put('/settings/api/webhook/{webhook_id}', 'Mpociot\CaptainHook\Http\WebhookController@update');
